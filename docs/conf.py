9fea0aa7129e949e256cfac11e0baf7c25901eca
adkendall <adkendall@hotmail.com>
2018-02-12 16:27:28 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -240,7 +240,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'ReadtheDocsTemplate', u'Read the Docs Template Documentation',
+  ('index', 'ReadtheDocsTemplate', u'DHP Documentation',
    u'Read the Docs', 'ReadtheDocsTemplate', 'One line description of project.',
    'Miscellaneous'),
 ]
