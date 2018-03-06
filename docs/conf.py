880a0288243db6bbf6d3d45aa6ff25f9c7450f5e
johnatggc <36697001+johnatggc@users.noreply.github.com>
2018-03-06 15:38:36 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -118,12 +118,12 @@ html_theme = 'default'
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 
-html_logo = '../img/178_x_292_Portal_Logo_without_Square.png'
+html_logo = '../img/300_x_330_Portal_Logo_with_Square.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = '../img/favicon.ico'
+#html_favicon = '../img/favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
